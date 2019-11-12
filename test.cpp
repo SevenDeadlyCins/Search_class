c6c78215b8c06f6cb1bba6032b512d1b33af3054
Chrimbous <56650757+Chrimbous@users.noreply.github.com>
2019-11-12 15:18:22 -0800

    Add files via upload
    The test files are the simplified version (without classes) of what I'm trying to do.

---
+++ test.cpp
@@ -0,0 +1,43 @@
+#include "test.h"
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+void searchDrinks(map<string, int>& drinkS, const string& name, map<string, int>& res)
+{
+	map<string, int>::iterator i = drinkS.begin();
+	map<string, int>::iterator it = drinkS.end();
+	for (i = drinkS.begin(); i != drinkS.end(); i++)
+	{
+		const string& key = i->first;
+		if (key.compare(0, key.length(), name, name.length()) == 0)
+		{
+			res.insert(pair<string, int>(i->first, i->second));
+		}
+	}
+}
+
+int main()
+{
+	map<string, int> teststore =
+	{
+		{"test123", 123},
+		{"test134", 134},
+		{"test245", 245},
+		{"test256", 256}
+	};
+	map<string, int> result;
+	string na;
+	while (na != "done")
+	{
+		cin >> na;
+		searchDrinks(teststore, na, result);
+		map<string, int>::const_iterator it = result.lower_bound(na);
+		while (result.empty() == false)
+		{
+			cout << it->first << " => " << it->second << endl;
+			result.erase(result.begin());
+		}
+	}
+}
