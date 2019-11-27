d3fad1503da2712638ce4f08a5a2875fd2777fda
Chrimbous <56650757+Chrimbous@users.noreply.github.com>
2019-11-26 21:04:52 -0800

    Add files via upload

--- Search.cpp
+++ Search.cpp
@@ -2,7 +2,7 @@
 
 //this function is given a map containing all drinks, a name of a drink to search for and will return anything that partially or completely matches 
 //what the user was searching for. This is returned by reference in a map of drinks.
-void Search::searchDrink(DrinkMap drinkStorage, const string& search, DrinkMap foundDrink)
+void Search::searchDrink(DrinkMap drinkStorage, const string& search, DrinkMap& foundDrink)
 {
 	DrinkMap::iterator i; //creates an iterator to easily access the key and value in drinkStorage. 
 	for (i = drinkStorage.begin(); i != drinkStorage.end(); i++) //loop that goes through the begining of drinkStorage to the end of drinkStorage.
@@ -18,44 +18,32 @@ void Search::searchDrink(DrinkMap drinkStorage, const string& search, DrinkMap f
 
 //this function is given a map containing all ingredients, a name of an ingredient to search for and will return anything that partially or completely matches 
 //what the user was searching for. This is returned by reference in a map of ingredients.
-void Search::searchIngredient(IngredientMap ingredientStorage, const string& search, IngredientMap foundIngredient)
+void Search::searchIngredient(DrinkMap drinkStorage, const string& search, DrinkMap& foundIngredient)
 {
-	IngredientMap::iterator i; //creates an iterator to easily access the key and value in ingredientStorage
-	for (i = ingredientStorage.begin(); i != ingredientStorage.end(); i++) //loop that goes through the begining of ingredientStorage to the end of ingredientStorage
+	int j;
+	bool found;
+	DrinkMap::iterator i; //creates an iterator to easily access the key and value in ingredientStorage
+	for (i = drinkStorage.begin(); i != drinkStorage.end(); i++) //loop that goes through the begining of ingredientStorage to the end of ingredientStorage
 	{
-		const string& key = i->first; //gets the key of the current point in ingredientStorage. used to compare the key with the user input
-		if (key.compare(0, search.size(), search, 0, search.size()) == 0) //compares the user input with the current key. explenation of what is happening below
-																	//if the first search.size() values in key are the same as the first search.size()values in name, then...
+		j = 0;
+		found = false;
+		do //loops to check the user input matches any of the ingredients in a drink  
 		{
-			foundIngredient.insert(IngredientPair(i->first, i->second)); //...insert the key and ingredient of the current point on map ingredientStorage into the map foundIngredient
-		}
+			const string& key = ingredientList.at(j).getName; //gets the current ingredient in the drink to compare with the search
+			if (key.compare(0, search.size(), search, 0, search.size()) == 0) //compares the user input with the current key. explenation of what is happening below
+																		//if the first search.size() values in key are the same as the first search.size()values in name, then...
+			{
+				foundIngredient.insert(DrinkPair(i->first, i->second)); //...insert the key and ingredient of the current point on map ingredientStorage into the map foundIngredient
+				found == true; //sets found to true, meaning that the current drink contains the ingredient searched for 
+			}
+			j++; //incrument j for the loop
+		}while (j < ingredientList.size && found == false); //if the number of ingredients is less than j (ingredient not found) or found == true (ingredient was found), exit loop
 	}
 }
 
-void Search::getSearch(string& search) //gets what the user wants to searh for and puts it into search.
+void Search::getSearch(DrinkMap drinkStorage, string& search, DrinkMap& drinkFound) //gets what the user wants to searh for and searches both for ingredients and drink names
 {
-	cin >> search;
+	cin >> search;//gets the users search
+	searchIngredient(drinkStorage, search, drinkFound); //searches for the drinks based on their ingredients
+	searchDrink(drinkStorage, search, drinkFound); //searches for the drinks based on the drink names, based on ingredient will be stored first
 }
-
-//Old versions of the search functions
-/*
-Drink Search::searchDrinks(string name) //Searches through a map of Drinks returning the first Drink that matches search, or nothing if it's not found.
-{
-	if (drinkStorage.count(name) == true)
-	{
-		return drinkStorage[name];
-	}
-	else
-		;
-}
-
-
-Ingredient Search::searchIngredient(string name) //Searhes through a map of Ingredients returning the first Ingredient that matches search, or nothing if not found.
-{
-	if (ingredientStorage.count(name) == true)
-	{
-		return ingredientStorage[name];
-	}
-	else
-		;
-}*/
--- Search.h
+++ Search.h
@@ -7,26 +7,20 @@ using namespace std;
 
 //typedef for maps and pairs for easier reading
 typedef map<string, Drink> DrinkMap;
-typedef map<string, Ingredient> IngredientMap;
 typedef pair<string, Drink> DrinkPair;
-typedef pair<string, Ingredient> IngredientPair;
 
 //Declaring Search Class
-class Search
+class Search: public Recipe
 {
 private:
 	DrinkMap drinkStorage; //Storage of all drinks
-	IngredientMap ingredientStorage; //Storage of all ingredients
 	DrinkMap foundDrink; //any drink that matches what was found in search, used to return multiple drinks
-	IngredientMap foundIngredient; //any Ingredient that matches what was found in search, used to return multiple ingredients 
 	string search; //what drink or ingredient the user is searching for
 public:
-	void searchDrink(DrinkMap drinkStorage, const string& search, DrinkMap foundDrink); //searches for drink(s) based on user input and will return drink(s) in a map by reference.
-	void searchIngredient(IngredientMap ingredientStorage, const string& search, IngredientMap foundIngredient); //searches for ingredient(s) based on user input and will return 
+	void searchDrink(DrinkMap drinkStorage, const string& search, DrinkMap& foundDrink); //searches for drink(s) based on user input and will return drink(s) in a map by reference.
+	void searchIngredient(DrinkMap drinkStorage, const string& search, DrinkMap& foundDrink); //searches for ingredient(s) based on user input and will return 
 																												  //ingredient(s) in a map by reference.
-	void getSearch(string& search); //gets what drink or ingredient the user wants to search. Will be used in both searchDrink and searchIngredient.
-	//Drink searchDrinks(string drName); //old version of searchDrink.
-	//Ingredient searchIngredient(string inName); //old version of searchIngredient.
-
+	void getSearch(DrinkMap drinkStorage, string& search, DrinkMap& drinkFound); //gets what drink or ingredient the user wants to search. Will be used in both searchDrink and searchIngredient.
+	friend class Recipie;
 };
 
